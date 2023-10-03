854a4747358e96237dbbd2e7b2e2af0edf019426
Jorge Perez <jorge_alberto1997@hotmail.com>
2023-10-02 20:52:07 -0600

    First version

---
+++ index.js
@@ -0,0 +1,17 @@
+function openUp() {
+  const $opentop = document.querySelector('#opentop');
+  const $top = document.querySelector('#top');
+  const $front = document.querySelector('#front');
+  const $back = document.querySelector('#back');
+  const $letter = document.querySelector('#letter');
+  const $button = document.querySelector('button');
+
+  // $opentop.beginElement();
+  $top.style.zIndex = 2;
+
+  $top.classList.add('animate');
+  $front.classList.add('animate');
+  $back.classList.add('animate');
+  $button.classList.add('animate');
+  $letter.classList.add('animate');
+}
